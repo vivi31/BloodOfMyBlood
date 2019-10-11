dd72429fdc4852f3918ab6302c33787afd265ebc
Alejandro-Perezg <55727945+Alejandro-Perezg@users.noreply.github.com>
2019-10-11 16:28:30 -0500

    Add files via upload
    ver 1.2 w/box2d

---
+++ BOMB2/desktop/src/mx/itesm/videojuegos/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mx.itesm.videojuegos.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mx.itesm.videojuegos.Juego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Juego(), config);
+	}
+}
